a244eeae93ce5adf5dcce9675b8a713b21ddf99a
הילה מעברי <hila.mabari@post.runi.ac.il>
2024-11-06 18:45:12 +0200

    Done

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,8 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		// To get you started, here is the first line in the program:
 	    // Replace this comment with the rest of your code   
 		String name2 = args[1];
 		String name3 = args[2];
