79dca7dbe9fd7c25f005018dacf558ca7df13b65
הילה מעברי <hila.mabari@post.runi.ac.il>
2024-11-08 10:23:41 +0200

    Done

--- Bill3.java
+++ Bill3.java
@@ -2,13 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name1 = args[0];   
 		String name2 = args[1];
 		String name3 = args[2];
 		int billTotal = Integer.parseInt(args[3]);
 		double billSplit = Math.ceil((double) billTotal/3);
-		System.out.println("Dear " + name1 + ", " +name2 + ", and " + name3 + 
-							": pay " + billSplit + " Shekels each.");  
+		System.out.println("Dear " + name3 + ", " +name2 + ", and " + name1 + 
+							": pay " + billSplit + " Shekels each");  
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,13 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		//Replace this comment with your code
 		int currentValue = Integer.parseInt(args[0]);
-		int rate = Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		double rateTypeDouble = Double.parseDouble(args[1]);
-		double futureValue = currentValue * Math.pow(rateTypeDouble/100 + 1, n);
-		System.out.println("After " + n + " years, $" + currentValue + " saved at " + 
-							rateTypeDouble + "% will yield $" + (int) futureValue);
+		double futureValue = currentValue * Math.pow(rate/100 + 1, n);
+		System.out.println("After " + n + " years, a $" + currentValue + " saved at " + 
+							rate + "% will yield $" + (int) futureValue);
 	}
 }
